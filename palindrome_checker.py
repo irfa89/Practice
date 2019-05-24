7587999c78f6a70a42ea2f67f1f6e5c191b60fd3
Shariq Khan <33172597+irfa89@users.noreply.github.com>
2019-05-24 06:01:50 -0400

    palindrome checker...

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -3,11 +3,23 @@ import sys
 import time
 
 def check_palindrome(input_str):
-    pass
+    d = deque.Deque()
+    for char in input_str:
+        d.add_rear(char)
 
+    while d.size() >= 2:
+        front_item = d.remove_front()
+        rear_item = d.remove_rear()
+
+        if front_item != rear_item:
+            return False
+
+    return True
 
 def main():
-    pass
+
+    n = str(input("Enter a string to check palindrome : "))
+    print(check_palindrome(n.lower()))
 
 if __name__ == "__main__":
     start = time.time()
